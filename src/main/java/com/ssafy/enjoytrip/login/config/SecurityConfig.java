ea930edfec53c822305db1336f6af4def4ba755f
전성호 <kopasd99@gmail.com>
2024-11-18 15:47:15 +0900

    chore : cors config 리팩토링

--- src/main/java/com/ssafy/enjoytrip/login/config/SecurityConfig.java
+++ src/main/java/com/ssafy/enjoytrip/login/config/SecurityConfig.java
@@ -44,24 +44,23 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setMaxAge(3600L);
+        configuration.addExposedHeader("Set-Cookie");
+        configuration.addExposedHeader("Authorization");
+        configuration.addExposedHeader("access");
+        return request -> configuration;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration configuration = new CorsConfiguration();
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
-                        configuration.setAllowedMethods(Collections.singletonList("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-                        configuration.setMaxAge(3600L);
-                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-                        return configuration;
-                    }
-                })));
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource())));
         http
                 .csrf((auth) -> auth.disable());
         http
