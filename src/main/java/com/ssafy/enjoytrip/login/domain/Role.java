4179b7a4bb4327ba8e5ca357d8bda953a682b273
전성호 <kopasd99@gmail.com>
2024-11-19 10:22:41 +0900

    feat : Enum 값을 @Valid로 검증하기 위해 custom 어노테이션 추가

--- src/main/java/com/ssafy/enjoytrip/login/domain/Role.java
+++ src/main/java/com/ssafy/enjoytrip/login/domain/Role.java
@@ -1,7 +1,6 @@
 package com.ssafy.enjoytrip.login.domain;
 public enum Role {
     ROLE_USER, ROLE_ADMIN, ROLE_GUEST;
-
     public static boolean isValidRole(String role) {
         try {
             Role.valueOf(role); // 이 메서드가 예외 없이 실행되면 유효한 값
