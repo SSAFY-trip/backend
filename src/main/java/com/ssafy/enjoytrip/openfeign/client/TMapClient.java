567156e2b86d031e0b4b6e7d411923119a48a7f7
spig0126 <spig0126@gmail.com>
2024-11-20 10:25:55 +0900

    Move: move clients to different package

--- src/main/java/com/ssafy/enjoytrip/openfeign/client/TMapClient.java
+++ src/main/java/com/ssafy/enjoytrip/openfeign/client/TMapClient.java
@@ -1,4 +1,4 @@
-package com.ssafy.enjoytrip.openfeign;
+package com.ssafy.enjoytrip.openfeign.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
