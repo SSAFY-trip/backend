8006725e990a97dfb747e47bda376ae8689ae5a6
spig0126 <spig0126@gmail.com>
2024-11-20 10:54:56 +0900

    Refractor: remove unused error code

--- src/main/java/com/ssafy/enjoytrip/global/exception/GlobalErrorCode.java
+++ src/main/java/com/ssafy/enjoytrip/global/exception/GlobalErrorCode.java
@@ -10,9 +10,6 @@ public enum GlobalErrorCode implements ErrorCode {
     VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "GLOABL_400_1", "Invalid data provided"),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GLOBAL_500_1", "Unexpected error in internal server has occurred."),
-//    INVALID_DATA(HttpStatus.BAD_REQUEST, "INVALID_DATA", "Invalid data provided"),
-//    MISSING_REQUIRED_FIELD(HttpStatus.BAD_REQUEST, "MISSING_REQUIRED_FIELD", "Required field is missing"),
-//    INVALID_FORMAT(HttpStatus.BAD_REQUEST, "INVALID_FORMAT", "Data format is incorrect"),
 
     S3_OPERATION_FAILED(HttpStatus.BAD_REQUEST, "S3_400_1", "S3 operation failed."),
     S3_FILE_NOT_FOUND(HttpStatus.BAD_REQUEST, "S3_400_2", "S3 file not found."),
